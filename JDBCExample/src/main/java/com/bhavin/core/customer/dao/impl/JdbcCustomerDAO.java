04198fe00929bfc6fa7a85e003a089f174b70aa8
Bhavin789 <bhavib.789@gmail.com>
2018-11-05 17:00:06 +0530

    Working code for insert query

--- JDBCExample/src/main/java/com/bhavin/core/customer/dao/impl/JdbcCustomerDAO.java
+++ JDBCExample/src/main/java/com/bhavin/core/customer/dao/impl/JdbcCustomerDAO.java
@@ -9,8 +9,36 @@ import com.bhavin.core.customer.dao.CustomerDAO;
 import com.bhavin.core.customer.model.Customer;
 
 public class JdbcCustomerDAO implements CustomerDAO{
+	
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+	
+	
 	public void insert(Customer customer){
+		String sql = "INSERT INTO customer " + "(CUST_ID, NAME, AGE) VALUES (?, ?, ?)";
+		Connection conn = null;
 		
+		try{
+			conn = dataSource.getConnection();
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ps.setInt(1, customer.getCustID());
+			ps.setString(2, customer.getName());
+			ps.setInt(3, customer.getAge());
+			ps.executeUpdate();
+			ps.close();
+			
+		}catch (SQLException e){
+			throw new RuntimeException(e);
+		}finally{
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {}
+			}
+		}
 	}
 	
 	public Customer findByCutomerID(int id){
